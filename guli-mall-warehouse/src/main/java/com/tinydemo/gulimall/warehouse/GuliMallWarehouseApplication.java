5d2aa14d913b34feba9822ec320ff69b3e13d3ae
LGP <651011260@qq.com>
2024-01-21 22:09:22 +0800

    init.

---
+++ guli-mall-warehouse/src/main/java/com/tinydemo/gulimall/warehouse/GuliMallWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.tinydemo.gulimall.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliMallWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuliMallWarehouseApplication.class, args);
+	}
+
+}
---
+++ guli-mall-warehouse/src/test/java/com/tinydemo/gulimall/warehouse/GuliMallWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinydemo.gulimall.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliMallWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
